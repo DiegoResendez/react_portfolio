b18343457fe2e90021379d0112438dacee21a3e0
unknown <diego.resendez@yahoo.com>
2020-09-13 18:58:47 -0700

    Updated portfolio

--- client/src/components/pages/portfolio.js
+++ client/src/components/pages/portfolio.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Project from "../project";
 
-const projects = [{ 
+const projects = [
+  {
+    title: "The Golf Club",
+    description: "Social media app to connect local golfers looking to play a round together.",
+    href: "https://uwbc-golfclub.herokuapp.com/",
+    img: "https://via.placeholder.com/150"
+  },
+    {
+    title: "Weather Dashboad",
+    description: "Weather Report Application with current temperature, humidity, wind speed and UV index ",
+    href: "https://diegoresendez.github.io/Weather_Dashboard/",
+    img: "https://via.placeholder.com/150"
+  },
+  { 
     title: "Work Day Scheduler",
     description: "Scheduling Application to help the user keep add and delete tasks for the work day",
-    href: "https://github.com/DiegoResendez/Work_Day_Scheduler",
+    href: "https://diegoresendez.github.io/Work_Day_Scheduler/",
     img: "https://via.placeholder.com/150"
   },
   {
@@ -14,27 +27,15 @@ const projects = [{
     img: "https://via.placeholder.com/150"
   },
   {
-    title: "Weather Dashboad",
-    description: "Weather Report Application with current temperature, humidity, wind speed and UV index ",
-    href: "https://diegoresendez.github.io/Weather_Dashboard/",
-    img: "https://via.placeholder.com/150"
-  },
-  {
-    title: "Camping Checklist",
-    description: "Checklist Application for a user to create a list of camping necessities.",
-    href: "https://github.com/DiegoResendez/Bootcamp_Project_2",
-    img: "https://via.placeholder.com/150"
-  },
-  {
     title: "Trivia Game",
     description: "Fun trivia application to challenge your StarWars knowledge",
     href: "https://diegoresendez.github.io/Code_Quiz/",
     img: "https://via.placeholder.com/150"
   },
   {
-    title: "MacGuyver Drinking App",
-    description: "Drink list application for those who are looking to make a drink from their own household items.",
-    href: "https://diegoresendez.github.io/MacGuyver_Drinking_App/",
+    title: "Camping Checklist",
+    description: "Checklist Application for a user to create a list of camping necessities.",
+    href: "https://github.com/DiegoResendez/Bootcamp_Project_2",
     img: "https://via.placeholder.com/150"
   },
   ]
