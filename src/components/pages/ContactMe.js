fad3e5d59d153bb757072e29a1452fddca8e7ab1
unknown <diego.resendez@yahoo.com>
2020-09-16 00:44:26 -0700

    Tuesday night Final Edits

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -33,40 +33,31 @@ class Contact extends React.Component{
                 <form onSubmit={this.mySubmitHandler}>
       {/* <h1>Hello {this.state.name}</h1> */}
       {/* <p>Enter your name, and submit:</p> */}
-      <ul>
- <li>
     <input
+        className="formList"
         type='text'
         name='name'
         placeholder='Name'
         onChange={this.myChangeHandler}
     />
-    </li>
-    <li>
     <input
         type='text'
         name='email'
         placeholder='Email'
         onChange={this.myChangeHandler}
     />
-    </li>
-    <li>
     <input
         type='text'
         name='subject'
         placeholder='Subject'
         onChange={this.myChangeHandler}
     />
-    </li>
-    <li>
-          <form>
+    <form>
       <textarea value={this.state.message} />
       </form>
       <input
         type='submit'
       />
-      </li>
-      </ul>
       </form>
                 </div>
         )
